87382e72bc100df5f8367d57104a543eb7b3f72c
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-20 15:43:49 +0300

    Create views for list of products and for product detail

--- teashop/shop/views.py
+++ teashop/shop/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    # only available products shown to customers
+    products = Product.objects.filter(available=True)
+    # in case if template have to show products within some category
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+# the main advantage of using slugs in this case is that
+# search engines have more chances to find such kind of
+# objects. Id is not enough for good SEO
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
