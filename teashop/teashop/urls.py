e71b218a10404a993d65dd5a9e692bd5dbdd0456
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-20 18:29:32 +0300

    add paths to MEDIA to urlpatterns

--- teashop/teashop/urls.py
+++ teashop/teashop/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
 ]
+
+# use it only in development, not after deployment
+# because Django don't have to work with static files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
