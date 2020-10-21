abffcf6ac0353fb54bb3ddf22979c2c48d85ede3
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-21 20:50:27 +0300

    Modify settings, add session_id for cart

--- teashop/teashop/settings.py
+++ teashop/teashop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Sessions
+
+CART_SESSION_ID = 'cart'
