e53d87c0b61b5e34e1ee91abb9d6429c8e5a696b
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-26 12:34:55 +0300

    Modify commentaries

--- teashop/cart/forms.py
+++ teashop/cart/forms.py
@@ -4,8 +4,8 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedMultipleChoiceField(choices=PRODUCT_QUANTITY_CHOICES, 
-                                              coerce=int) # automatically convert value into int
+    quantity = forms.TypedMultipleChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
+                                              coerce=int) #automatically convert value into int (doesn't work)
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
