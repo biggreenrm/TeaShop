b1579678b778ceffe616d9deb9e39364b17ac9ae
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-26 20:13:32 +0300

    Create view for customer form for making order

--- teashop/orders/views.py
+++ teashop/orders/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrdersCreateForm
+from cart.cart import Cart
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrdersCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+    else:
+        form = OrdersCreateForm()
+    return render()
+        
