60f9d1ee94d873614a92c51aad60af776e1b761b
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-26 16:43:29 +0300

    Register model Orders in admin panel

--- teashop/orders/admin.py
+++ teashop/orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Orders, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+    
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name',
+                    'email', 'adress', 'postal_code',
+                    'city', 'paid', 'created',
+                    'updated']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
