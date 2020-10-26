f8d90485e851e1c59139f60f8a944a5991862dc3
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-26 16:35:12 +0300

    Create models for orders

--- teashop/orders/models.py
+++ teashop/orders/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from shop.models import Product
 
-# Create your models here.
+
+class Orders(models.Model):
+    """
+    Model that consist info about orders.
+    """
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    adress = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+        
+    def __str__(self):
+        return f'Order {self.id}'
+    
+    def get_total_sum(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+
+class OrderItem(models.Model):
+    """
+    
+    """
+    
+    order = models.ForeignKey(Orders,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return f'{self.id}'
+    
+    def get_cost():
+        return self.price * self.quantity
