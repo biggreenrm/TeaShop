89672eea01f1c22f0b93d120f613c28d405cc3a2
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-27 10:05:32 +0300

    Create view for creating order

--- teashop/orders/views.py
+++ teashop/orders/views.py
@@ -10,13 +10,21 @@ def order_create(request):
         form = OrdersCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
+            # bind every item in cart to customers order
+            # information is get from cart also
             for item in cart:
                 OrderItem.objects.create(order=order,
                                          product=item['product'],
                                          price=item['price'],
                                          quantity=item['quantity'])
+            # after making an order there is no need to keep cart
             cart.clear()
+            return render(request,
+                          'orders/order/created.html',
+                          {'order': order})
     else:
         form = OrdersCreateForm()
-    return render()
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
         
