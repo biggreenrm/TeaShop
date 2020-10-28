e904123c3382ba393650515ba3eb1bbc4018c74a
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-28 13:37:55 +0300

    Modify orders model by adding new field for braintree_id

--- teashop/orders/models.py
+++ teashop/orders/models.py
@@ -16,6 +16,7 @@ class Orders(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    braintree_id = models.CharField(max_length=150, blank=True)
 
     class Meta:
         ordering = ('-created',)
