b9cdc0a9e5c65cc243c1bc10a0337d321cced277
SergeyGurkov <biggreen.rm@gmail.com>
2020-10-31 15:20:12 +0300

    Upload all static files (doesn't help)

--- teashop/teashop/settings.py
+++ teashop/teashop/settings.py
@@ -124,6 +124,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Sessions
