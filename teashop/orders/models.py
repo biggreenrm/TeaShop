64616d0227fcbed960edfe0bac07898f35c21a80
SergeyGurkob <biggreen.rm@gmail.com>
2020-11-10 18:22:08 +0300

    Modify model Orders by adding two more related field (Coupon) and discount. Also modify class function get_total_sum to work with discounts

--- teashop/orders/models.py
+++ teashop/orders/models.py
@@ -1,5 +1,11 @@
+# Django stuff
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+# First-party Models
+from coupons.models import Coupon
 from shop.models import Product
+# Third-party
+from decimal import Decimal
 
 
 class Orders(models.Model):
@@ -17,6 +23,14 @@ class Orders(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     braintree_id = models.CharField(max_length=150, blank=True)
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0),
+                                               MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created',)
@@ -25,7 +39,8 @@ class Orders(models.Model):
         return f'Order {self.id}'
     
     def get_total_sum(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))
     
 
 class OrderItem(models.Model):
