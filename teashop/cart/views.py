7c98a406aa8a909245d113ea368dac23ade43d67
SergeyGurkob <biggreen.rm@gmail.com>
2020-11-10 18:55:14 +0300

    Modify redirects in product detail. Now they redirect to the same page after adding product to cart.

--- teashop/cart/views.py
+++ teashop/cart/views.py
@@ -16,7 +16,7 @@ def cart_add(request, product_id):
         cart.add(product=product,
                  quantity=cd['quantity'],
                  update_quantity=cd['update'])
-        return redirect('cart:cart_detail')
+        return redirect('shop:product_detail', product.id, product.slug)
 
 def cart_remove(request, product_id):
     cart = Cart(request)
