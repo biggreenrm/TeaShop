21b4e87672db2098da8aae4a26c86edc0d2f7c36
SergeyGurkob <biggreen.rm@gmail.com>
2020-11-10 15:59:37 +0300

    Add dockerfile for Docker, fill up the requrements.txt and change ALLOWED_HOSTS to all

--- teashop/teashop/settings.py
+++ teashop/teashop/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '%sp_bd)qe#*k0go$z0pri*i=ueflgo7u0#a42pe7$8zvn1-51i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
